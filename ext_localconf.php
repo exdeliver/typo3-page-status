024792db64653b44fb56f776e1f1d3d931ed7a2e
Jason  Hoendervanger <renalpha@hotmail.com>
2026-04-08 22:27:00 +0200

    removal tests

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,3 @@
 <?php
 
-declare(strict_types=1);
-
-defined('TYPO3') or die();
-
 $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['page_status'] = 'EXT:page_status/Resources/Public/Css/page-status.css';
